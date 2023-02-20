58a66b5caef91bffcd4b46491c0759ceeff24ffb
杜宇 <dygood@outlook.com>
2023-02-20 12:23:08 +0800

    first commit

---
+++ src/EasilyNET.Core/Enums/EGender.cs
@@ -0,0 +1,15 @@
+﻿namespace EasilyNET.Core.Enums;
+/// <summary>
+/// 性别枚举
+/// </summary>
+public enum EGender
+{
+    /// <summary>
+    /// 女
+    /// </summary>
+    女 = 0,
+    /// <summary>
+    /// 男
+    /// </summary>
+    男 = 1
+}
---
+++ src/EasilyNET.Core/InfoItems/IdNameItem.cs
@@ -0,0 +1,37 @@
+﻿// ReSharper disable UnusedMember.Global
+// ReSharper disable MemberCanBePrivate.Global
+namespace EasilyNET.Core;
+/// <summary>
+/// 包含Id和Name字段的对象
+/// </summary>
+public class IdNameItem
+{
+    /// <summary>
+    /// 构造函数
+    /// </summary>
+    public IdNameItem() { }
+    /// <summary>
+    /// 构造函数
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="name"></param>
+    public IdNameItem(string id, string name) { Id = id; Name = name; }
+    /// <summary>
+    /// ID
+    /// </summary>
+    public string Id { get; set; } = string.Empty;
+    /// <summary>
+    /// Name
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+    /// <summary>
+    /// 转化成ReferenceItem对象
+    /// </summary>
+    /// <returns>ReferenceItem</returns>
+    public ReferenceItem GetReferenceItem() => new(Id, Name);
+    /// <summary>
+    /// 获取Name值
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString() => Name;
+}
